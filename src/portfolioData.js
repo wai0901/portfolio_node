00f398bb825749fe2ba1c61ba675b4252a7a1114
Kent leung <snail@hotmail.com>
2020-07-19 01:00:23 -0700

    updated link

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -4,6 +4,7 @@ const portfolioData = [
     linkId: 'toP1',
     id: 's1',
     dataDot: 'd1',
+    image: 'img/Luke_website.jpg',
     youTubeLink: 'https://www.youtube.com/embed/skC5g5xHqeI',
     title: 'Luke Website',
     tools: 'React, Redux, React-Router, Thunk, Material-UI, Mongo, Mogoose, AWS EC2, Functional shopping cart by fetching and pushing data from AWS server and MongoDB data base, Validation control form',
@@ -12,22 +13,76 @@ const portfolioData = [
                     'https://github.com/wai0901/luke-web-express',
                     'https://github.com/wai0901/luke-web-express-server'
                 ],
-    fontAwesomeIcon: 'fab fa-github-square'
     },
     {
-    time: 'moon',
+    time: 'sun',
     linkId: 'toP2',
     id: 's2',
     dataDot: 'd2',
-    youTubeLink: 'https://www.youtube.com/embed/skC5g5xHqeI',
-    title: 'Luke Website',
-    tools: 'React, Redux, React-Router, Thunk, Material-UI, Mongo, Mogoose, AWS EC2, Functional shopping cart by fetching and pushing data from AWS server and MongoDB data base, Validation control form',
-    websiteLink: 'https://lukeexpress.com/',
+    image: 'img/Luke_mobile.jpg',
+    youTubeLink: 'https://www.youtube.com/embed/XgtrfHOeHM0',
+    title: 'Luke iOS and Android App',
+    tools: 'React Native Expo, React Navigation, Redux, Firebase, Functional shopping cart by fetching and pushing data from Firebase server',
+    websiteLink: '#',
     githubLink: [
-                    'https://github.com/wai0901/luke-web-express',
-                    'https://github.com/wai0901/luke-web-express-server'
+                    'https://github.com/wai0901/Luke-react-native'
                 ],
-    fontAwesomeIcon: 'fab fa-github-square'
+    },
+    {
+    time: 'moon',
+    linkId: 'toP3',
+    id: 's3',
+    dataDot: 'd3',
+    image: 'img/covid19.jpg',
+    youTubeLink: 'https://www.youtube.com/embed/p50kCbgTFEk',
+    title: 'Covid-19 Tracker',
+    tools: 'React, Chart.js, Axios, Postman Covid-19 API',
+    websiteLink: 'https://covid-19-live-tracker.herokuapp.com',
+    githubLink: [
+                    'https://github.com/wai0901/Covid19tracker'
+                ],
+    },
+    {
+    time: 'sun',
+    linkId: 'toP4',
+    id: 's4',
+    dataDot: 'd4',
+    image: 'img/portfolio-web.jpg',
+    youTubeLink: 'https://www.youtube.com/embed/skC5g5xHqeI',
+    title: 'My Web Porfolio Page',
+    tools: 'html, sass, javascript, nodejs, express',
+    websiteLink: '#',
+    githubLink: [
+                    'https://github.com/wai0901/Porfolio'
+                ],
+    },
+    {
+    time: 'moon',
+    linkId: 'toP5',
+    id: 's5',
+    dataDot: 'd5',
+    image: 'img/kaffa_image.jpg',
+    youTubeLink: 'https://www.youtube.com/embed/eFk6Hg9HYS8',
+    title: 'Kaffa Website',
+    tools: 'html, css, javascript, bootstrap, jQuery',
+    websiteLink: 'https://wai0901.github.io/wonagseged_kaffa',
+    githubLink: [
+                    'https://github.com/wai0901/wonagseged_kaffa'
+                ],
+    },
+    {
+    time: 'sun',
+    linkId: 'toP6',
+    id: 's6',
+    dataDot: 'd6',
+    image: 'img/Weather_Webapp.jpg',
+    youTubeLink: 'https://www.youtube.com/embed/skC5g5xHqeI',
+    title: 'Weather Web App',
+    tools: 'html, css, javascript, react, nodejs',
+    websiteLink: 'https://weather-webapp.netlify.com',
+    githubLink: [
+                    'https://github.com/wai0901/weather-webapp'
+                ],
     },
 ]
 
